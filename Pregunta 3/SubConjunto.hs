a114135a3e9ee7f4536ac753f8b5855ebf678ffc
napoleonbaley2011 <113143033+napoleonbaley2011@users.noreply.github.com>
2022-12-01 15:38:04 -0400

    Update SubConjunto.hs

--- Pregunta 3/SubConjunto.hs
+++ Pregunta 3/SubConjunto.hs
@@ -1,3 +1,11 @@
 subconjunto :: Eq a => [a] -> [a] -> Bool
 subconjunto xs ys = 
   [x | x <- xs, x `elem` ys] == xs
+  
+ //prueba
+ :load subconjunto.hs
+subconjunto [1,5,8,7] [1,5,2,8,6,7,4]
+"true"
+
+subconjunto [1,4,8] [1,2,4,6]
+"false"
