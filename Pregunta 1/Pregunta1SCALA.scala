ffd12b68db397594ebe6f90e64c1c22bb104f6bb
napoleonbaley2011 <113143033+napoleonbaley2011@users.noreply.github.com>
2022-12-01 15:32:33 -0400

    Add files via upload

---
+++ Pregunta 1/Pregunta1SCALA.scala
@@ -0,0 +1,32 @@
+object Main extends App{
+    //UNIV. MACUSAYA AGUILAR YURIKA
+	//PREGUNTA 1
+    //FIBONACCI RECURSIVO
+    def fiboRecursive(n: Int): Int = {
+        if      (n < 2) n
+        else    fiboRecursive(n-1) + fiboRecursive(n-2)
+    }
+    //FIBONACCI ITERATIVO
+    def fiboIter(n: Int): Int = {
+        var a = -1
+        var b = 1
+        for (i <- 0 to n){
+            var buffer = a
+            a = b
+            b = buffer + b
+            print(b)
+        }
+            println()
+        a
+    }
+
+    def main(args:Array[String]):Unit = {
+	    //PREGUNTA 1
+	    //fIBONACCI RECURSIVO Y ESTRUCTURADO-
+	    println(fiboRecursive(7))
+	    fiboIter(7)
+
+	    
+    }
+    
+}
